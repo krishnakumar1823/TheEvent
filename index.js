b59a0ed28424aa162733d492932bd8076772df42
krishnakumar1823 <kumarkrishna180320@gmail.com>
2023-09-25 20:19:38 +0530

    update

---
+++ index.js
@@ -0,0 +1,8 @@
+function openMenuBar(){
+    var menubar=document.getElementById("menubar")
+    menubar.classList.add("menubarBlock")
+}
+function menubarClose(){
+    var menubar=document.getElementById("menubar")
+    menubar.classList.remove("menubarBlock")
+}
